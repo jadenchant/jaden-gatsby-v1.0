7c335f01033ec2b9d355df81f27c85a4c80ad9dd
Jaden Chant <chant1@stolaf.edu>
2021-03-17 18:57:32 -0500

    somewhat working hamberger

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 import "../css/Header.css";
 
 const Header = () => {
+  const [dropdown, setDropdown] = useState(false);
+  const [hamberger, setHamberger] = useState("nav-hamberger-icon");
+
+  const handleClick = () => {
+    const defaultHamberger = "nav-hamberger-icon";
+    if (dropdown) {
+      setDropdown(false);
+      setHamberger(defaultHamberger);
+      console.log("dropdown set false");
+    } else {
+      setDropdown(true);
+      setHamberger(defaultHamberger + " open");
+      console.log("dropdown set true");
+    }
+  };
+
   return (
     <main className='header'>
-      <div className='nav-hamberger'>
-        <span className='nav-hamberger-icon'></span>
+      <div className='nav-hamberger' onClick={handleClick}>
+        <span className={hamberger}></span>
       </div>
       <div className='title'>{/* <h1>Jaden Chant</h1> */}</div>
     </main>
