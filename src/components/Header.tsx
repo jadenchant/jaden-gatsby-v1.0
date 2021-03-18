57f201652e852c613cddde6a78051ef47a642d11
Jaden Chant <chant1@stolaf.edu>
2021-03-17 20:49:10 -0500

    added navbar without fancy animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,16 +5,20 @@ import "../css/Header.css";
 const Header = () => {
   const [dropdown, setDropdown] = useState(false);
   const [hamberger, setHamberger] = useState("nav-hamberger-icon");
+  const [nav, setNav] = useState("nav initial");
 
   const handleClick = () => {
     const defaultHamberger = "nav-hamberger-icon";
+    const defaultNav = "nav";
     if (dropdown) {
       setDropdown(false);
       setHamberger(defaultHamberger + " close");
+      setNav(defaultNav + " initial");
       console.log("dropdown set false");
     } else {
       setDropdown(true);
       setHamberger(defaultHamberger + " open");
+      setNav(defaultNav + " final");
       console.log("dropdown set true");
     }
   };
@@ -34,6 +38,34 @@ const Header = () => {
         </div>
         <div className='title'>{/* <h1>Jaden Chant</h1> */}</div>
       </header>
+      <div className={nav}>
+        <ul className='nav-items'>
+          <Link
+            to='/'
+            draggable='false'
+            className='nav-item'
+            activeClassName='current'
+          >
+            Home
+          </Link>
+          <Link
+            to='/about'
+            draggable='false'
+            className='nav-item'
+            activeClassName='current'
+          >
+            About
+          </Link>
+          <Link
+            to='/portfolio'
+            draggable='false'
+            className='nav-item'
+            activeClassName='current'
+          >
+            Portfolio
+          </Link>
+        </ul>
+      </div>
     </main>
   );
 };
---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+import "../css/portfolio.css";
+
+const Portfolio = () => {
+  return (
+    <main className='portfolio'>
+      <Header />
+    </main>
+  );
+};
+
+export default Portfolio;
