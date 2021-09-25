f8f56d5c21ade26867ba97cf1aca8e4885d5921e
Jaden Chant <chant1@stolaf.edu>
2021-09-24 22:28:43 -0500

    updated tailwind index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,8 @@ const IndexPage = () => {
       <Head title='Home' description='homepage' kewords='home' />
       <Header />
       <section className='content'>
-        <h1 className='title'>Home</h1>
-        <div className='intro'>
+        <h1 className='text-3xl px-8 py-10 font-bold uppercase'>Home</h1>
+        <div className='text-2xl px-8 py-0 text-center'>
           <p>
             Hello, I'm Jaden Chant. I am an aspiring Web Developer. I love to
             learn new skills and develop beautiful websites.
